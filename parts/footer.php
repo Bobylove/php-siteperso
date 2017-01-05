b5277dac723eacd8c311a6edd71d46d257f19267
Fred <delon.frederic.pro.job@gmail.com>
2017-01-05 16:08:09 +0100

    link css et app js

--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,7 @@
+<?php
+include "app.js"; 
+?>
 <h3>Footer ICI</h3>
+<script src="./public/app.js"></script>
 </body>
 </html>
--- parts/header.php
+++ parts/header.php
@@ -1,8 +1,12 @@
+<?php
+include "menu.php"; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="./public/style.css">
 </head>
 <body>
 	
