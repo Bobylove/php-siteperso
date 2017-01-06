7e5ac1921eeb8943c6eb92aba1a9693585c4e43f
Fred <delon.frederic.pro.job@gmail.com>
2017-01-06 10:27:05 +0100

    création du formulaire

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,18 @@
-<h3>Contact ici</h3>
+<div class="contact">
+	<h3>Contact ici</h3>
+	<form action="save.php" method="POST">
+		<label for="name"> Prénom
+			<input type="text" placeholder="jean" id="name" value="">
+		</label>
+		<label for="firstName"> Nom
+			<input type="text" placeholder="dupont" id="firstName" value="">
+		</label>
+		<label for="Entreprise"> Entreprise
+			<input type="text" placeholder="simplon" id="Entreprise" value="">
+		</label>
+		<!-- <label for="experience"> Experience
+			<input type="text" placeholder="exp" id="experience">
+		</label> -->
+		<button class="save">Save</button>
+	</form>
+</div>
