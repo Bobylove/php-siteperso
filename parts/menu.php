c9fb0118ac93638275ad4de3ff7d96777cf80d23
Fred <delon.frederic.pro.job@gmail.com>
2017-01-06 09:43:52 +0100

    ajout des liens href

--- parts/menu.php
+++ parts/menu.php
@@ -2,13 +2,13 @@
 	<menu>
 		<ul>
 			<li>
-			<h3><a href="../pages/bio.php?page=home">accueil</a></h3>	
+			<h3><a href="index.php?page=home">accueil</a></h3>	
 			</li>
 			<li>
-				<h3><a href="../pages/bio.php?page=bio">bio</a></h3>
+				<h3><a href="index.php?page=bio">bio</a></h3>
 			</li>
 			<li>
-				<h3><a href="../pages/bio.php?page=contact">contact</a></h3>
+				<h3><a href="index.php?page=contact">contact</a></h3>
 			</li>
 		</ul>
 	</menu>
